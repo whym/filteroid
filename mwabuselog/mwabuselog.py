a194f82bfb02dc5c6c79bf8bb10fe1ca3fb77d16
whym <whym@whym.org>
2010-11-17 19:30:02 +0900

    * mark/remove missing entries, etc

--- mwabuselog/mwabuselog.py
+++ mwabuselog/mwabuselog.py
@@ -57,12 +57,24 @@ def is_valid_uri(s):
     return s.index(':') > 0
 
 def get_sitename(mwhome):
-    url = '%s/w/api.php?format=xml&action=query&meta=siteinfo' % mwhome
-    doc = minidom.parseString(urllib.urlopen(url).read())
+    doc = minidom.parseString(urllib.urlopen(mwhome + '/w/api.php', urllib.urlencode(
+                {'format': 'xml',
+                 'action': 'query',
+                 'meta':  'siteinfo'}
+                )).read())
     elems = doc.getElementsByTagName('general')
     if len(elems) > 0:
         return '%s (%s)' % (elems[0].attributes['sitename'].value, elems[0].attributes['wikiid'].value)
     return None
+
+def get_missing_titles(mwhome, titles):
+    doc = minidom.parseString(urllib.urlopen(mwhome + '/w/api.php', urllib.urlencode(
+                {'format': 'xml',
+                 'action': 'query',
+                 'titles':  '|'.join(titles).encode('utf-8')}
+                )).read())
+    elems = doc.getElementsByTagName('page')
+    return map(lambda x: x.attributes['title'].value, filter(lambda x: x.attributes.has_key('missing'), elems))
 
 if __name__ == '__main__':
     # TODO: エラーの時は text/plain などにする
@@ -75,6 +87,7 @@ if __name__ == '__main__':
     targets = 'result|title|filter|timestamp|ids'
     filter_results = re.compile('^(warn|)$')
     shortener = None
+    skip_missing = False
 
     args = get_args()
     if args.has_key('mwhome'):
@@ -91,6 +104,8 @@ if __name__ == '__main__':
             shortener = None
     if args.has_key('filter'):
         filter_results = re.compile(args['filter'])
+    if args.has_key('skip_missing'):
+        skip_missing = True
 
     if not mwhome:
         print 'Content-Type: text/html'
@@ -108,21 +123,37 @@ if __name__ == '__main__':
 
     sitename = get_sitename(mwhome)
     # TODO: remove illegal arguments
-    uri = "%s/w/api.php?format=xml&action=query&list=abuselog&afllimit=%s&aflprop=%s" % (mwhome, limit, targets)
-    itemtag = 'item'
-    uri = urllib.urlopen(uri)
-    host = urlparse.urlparse(uri.geturl()).hostname
+    uri = urllib.urlopen((mwhome + '/w/api.php'), urllib.urlencode(
+            {'format': 'xml',
+             'action': 'query',
+             'list':  'abuselog',
+             'afllimit': str(limit),
+             'aflprop': str(targets)}
+            ))
     xmlstr = uri.read()
     doc = minidom.parseString(xmlstr)
-    items = {}#Sat, 13 Nov 2010 09:59:30 GMT
+    items = {}
 
     # 'ids' corresponds to 'id' and 'filter_id'
     targets = targets.replace('ids', 'id|filter_id')
 
     formatsize = len(format.split('%s')) - 1
-    
-    for item in doc.getElementsByTagName(itemtag):
+    for item in doc.getElementsByTagName('item'):
+        logid = item.attributes['id'].value
+        items[logid] = item
+    missing_titles = {}
+    for title in get_missing_titles(mwhome, map(lambda x: x.attributes['title'].value, items.values())):
+        missing_titles[title] = True
+
+    emit_items = {}
+    for (logid,item) in items.items():
+        title = item.attributes['title'].value
         formatted = format % tuple([item.attributes[x].value for x in targets.split('|')][0:formatsize])
+        if missing_titles.has_key(title):
+            if skip_missing:
+                continue
+            else:
+                formatted = '*' + formatted
 
         # pass only the items with pre-defined results
         result = item.attributes['result'].value or ''
@@ -130,19 +161,17 @@ if __name__ == '__main__':
             None
         else:
             continue
+        pagelink = '%s/wiki/%s' % (mwhome, urllib.quote(title.encode('utf-8')))
+        if shortener:
+            pagelink = shorten_url(pagelink.encode('utf-8'), shortener)
+        formatted = formatted.replace('__PAGE__', pagelink)
 
-        if item.attributes.has_key('title'):
-            pagelink = '%s/wiki/%s' % (mwhome, urllib.quote(item.attributes['title'].value.encode('utf-8')))
-            if shortener:
-                pagelink = shorten_url(pagelink.encode('utf-8'), shortener)
-            formatted = formatted.replace('__PAGE__', pagelink)
-        logid = item.attributes['id'].value
         pubdate = datetime.datetime.strptime(item.attributes['timestamp'].value, "%Y-%m-%dT%H:%M:%SZ")
         pubdate = pubdate.strftime("%a, %d %b %Y %H:%M:%S GMT")
         link = '%s/wiki/Special:AbuseLog/%s' % (mwhome, logid)
         if shortener:
             link = shorten_url(link.encode('utf-8'), shortener)
-        items[logid] = {
+        emit_items[logid] = {
             'description': formatted,
             'link': link,
             'pubDate': pubdate,
@@ -155,10 +184,10 @@ if __name__ == '__main__':
             print '%s: %s' % (header, x if x else default)
     
     import dict2rss
-    content = dict2rss.dict2rss({'title': sitename + ' AbuseLog',
+    content = dict2rss.dict2rss({'title': sitename + ' AbuseLog ' + filter_results.pattern,
                                  'description': 'auto-generated from the data available at ' + mwhome + '/wiki/Special:AbuseLog',
                                  'link': 'http://filteroid.appspot.com',
-                                 'item': items})._out().encode('utf-8')
+                                 'item': emit_items})._out().encode('utf-8')
     printheader('Last-Modified', uri)
     printheader('Content-Type', uri, 'text/xml; charset="UTF-8"')
     print ''
